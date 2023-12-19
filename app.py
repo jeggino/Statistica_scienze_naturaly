82d36654b144c3a22ce4c309ef6a927a8550700b
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-12-20 00:39:45 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="statistica_SN",
+    page_icon="🪶",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        'About': "# This is a header. This is an *extremely* cool app!"
+    }
+)
