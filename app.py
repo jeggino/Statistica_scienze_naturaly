c1ee0908c04fafc22dc993d04116a92a54113138
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-12-20 00:46:00 +0100

    Update app.py

--- app.py
+++ app.py
@@ -9,3 +9,15 @@ st.set_page_config(
         'About': "# This is a header. This is an *extremely* cool app!"
     }
 )
+
+with st.sidebar:
+    choose = option_menu("App Gallery", ["About", "Photo Editing", "Project Planning", "Python e-Course", "Contact"],
+                         icons=['house', 'camera fill', 'kanban', 'book','person lines fill'],
+                         menu_icon="app-indicator", default_index=0,
+                         styles={
+                            "container": {"padding": "5!important", "background-color": "#fafafa"},
+                            "icon": {"color": "orange", "font-size": "25px"}, 
+                            "nav-link": {"font-size": "16px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
+                            "nav-link-selected": {"background-color": "#02ab21"},
+                        }
+                        )
