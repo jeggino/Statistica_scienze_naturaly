fcdf0bdbe1e2ca7239397252d79b392efb8a3514
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-12-20 00:54:56 +0100

    Update app.py

--- app.py
+++ app.py
@@ -13,12 +13,12 @@ st.set_page_config(
 )
 
 with st.sidebar:
-    choose = option_menu("App Gallery", ["About", "Photo Editing", "Project Planning", "Python e-Course", "Contact"],
-                         icons=['house', 'camera fill', 'kanban', 'book','person lines fill'],
+    choose = option_menu("Menú", ["Teoria", "Esempi", "Prova con I tuoi dati"],
+                         icons=['book', 'bi-lightbulb', 'kanban',],
                          menu_icon="app-indicator", default_index=0,
                          styles={
                             "container": {"padding": "5!important", "background-color": "#fafafa"},
-                            "icon": {"color": "orange", "font-size": "25px"}, 
+                            "icon": {"color": "red", "font-size": "25px"}, 
                             "nav-link": {"font-size": "16px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
                             "nav-link-selected": {"background-color": "#02ab21"},
                         }
