88f135803af5a3559881bb4dde6a5e5160c3ab00
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-12-20 10:34:32 +0100

    Update app.py

--- app.py
+++ app.py
@@ -17,6 +17,13 @@ choose = option_menu(None,["Teoria", "Esempi", "Prova con I tuoi dati"],
                      icons=['book', 'bi-lightbulb', 'kanban',],
                      menu_icon="app-indicator", default_index=0,
                       orientation="horizontal",
+                     
+                     styles={
+        "container": {"padding": "0!important", "background-color": "#fafafa"},
+        "icon": {"color": "orange", "font-size": "25px"}, 
+        "nav-link": {"font-size": "25px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
+        "nav-link-selected": {"background-color": "green"},
+    }
                      styles={
                         "container": {"padding": "5!important"},
                         "icon": {"color": "red", "font-size": "25px"}, 
