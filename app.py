7c79655595101629ddfcbf78f20cb5d8a513a110
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-12-20 01:32:29 +0100

    Update app.py

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ if choose == "Teoria":
             kmq, ecc.."""
 
     latex = r""" + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =\sum_{k=0}^{n-1} ar^k =a \left(\frac{1-r^{n}}{1-r}\right)"""
-    latex = r"""P(X = x) = """
+    latex = r"""P(X = x) = \lambda ^ x"""
     
     st.markdown(body, unsafe_allow_html=False,  help=None)
     st.latex(latex, help=None)
